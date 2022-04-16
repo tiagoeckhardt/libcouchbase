2169f2bd79d6f4d780b6e95cd956c307512edf3e
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-04-16 22:15:02 +0200

    Update insert.sh

--- .github/workflows/insert.sh
+++ .github/workflows/insert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cbc n1ql -u 475bb1f6e6c842ff -P af6304aae4ec2db9 -U https://cb-21460.couchbase.live:28091/information 'INSERT INTO information (key,value) VALUES ("emp_1234",{"empno": 1234,"ename": "COUNT","job": "MUPPET","mgr": 7902,"hiredate": "1989/10/15","sal": 900,"comm": 0,"dept": { "deptno": 20, "dname": "RESEARCH", "loc": "DALLAS" }}) RETURNING *'
+cbq -u 475bb1f6e6c842ff -p af6304aae4ec2db9 -e "http://cb-21460.couchbase.live:28091" --script="'INSERT INTO CRUD_Lab (key,value) VALUES ("emp_1234",{"empno": 1234,"ename": "COUNT","job": "MUPPET","mgr": 7902,"hiredate": "1989/10/15","sal": 900,"comm": 0,"dept": { "deptno": 20, "dname": "RESEARCH", "loc": "DALLAS" }}) RETURNING *'"
